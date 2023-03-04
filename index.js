84e07979ac65571b9a53992f1335666b0e4b3935
Marino Adames <marinoadames85@gmail.com>
2023-03-03 19:53:13 -0500

    advanced learning

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const drivers = ['Antonio', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, -2)
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function (farePerRide){
+    return function(howManyFaresNeeded){
+        return farePerRide * howManyFaresNeeded
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, theOtherFunction){
+    return theOtherFunction(drivers)
+}
